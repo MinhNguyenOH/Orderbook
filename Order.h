798b7d07cb919e8881f0ceb8cd7bd007eb79ebc9
Minh Nguyen <minhnguyen@Minhs-MacBook-Pro.local>
2024-05-26 22:24:44 -0400

    Updated header files

---
+++ Order.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <list>
+#include <exception>
+#include <format>
+#include <sstream>
+#include <iostream>
+
+#include "OrderType.h"
+#include "Side.h"
+#include "Usings.h"
+
+class Order
+{
+public:
+    Order(OrderType orderType, OrderId orderId, Side side, Price price, Quantity quantity)
+        : orderType_{ orderType }
+        , orderId_{ orderId }
+        , side_{ side }
+        , price_{ price }
+        , initialQuantity_{ quantity }
+        , remainingQuantity_{ quantity }
+    { }
+
+    OrderId GetOrderId() const { return orderId_; }
+    Side GetSide() const { return side_; }
+    Price GetPrice() const { return price_; }
+    OrderType GetOrderType() const { return orderType_; }
+    Quantity GetInitialQuantity() const { return initialQuantity_; }
+    Quantity GetRemainingQuantity() const { return remainingQuantity_; }
+    Quantity GetFilledQuantity() const { return GetInitialQuantity() - GetRemainingQuantity(); }
+    bool IsFilled() const { return GetRemainingQuantity() == 0; }
+    void Fill(Quantity quantity)
+    {
+        if (quantity > GetRemainingQuantity())
+            std::ostringstream oss;
+            oss << "Order (" << GetOrderId() << ") cannot be filled for more than its remaining quantity.";
+            throw std::logic_error(oss.str());
+        remainingQuantity_ -= quantity;
+    }
+
+private:
+    OrderType orderType_;
+    OrderId orderId_;
+    Side side_;
+    Price price_;
+    Quantity initialQuantity_;
+    Quantity remainingQuantity_;
+};
+
+using OrderPointer = std::shared_ptr<Order>;
+using OrderPointers = std::list<OrderPointer>;
